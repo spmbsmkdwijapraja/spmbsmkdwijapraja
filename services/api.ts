adbb5cf0f3328810e3e24f6c7836d0b44cc299bb
spmbsmkdwijapraja <ppdbsmkdwijapraja@gmail.com>
2026-04-03 13:20:06 +0700

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -5,7 +5,7 @@ import { StudentRegistration } from '../types';
  * GANTI URL DI BAWAH INI dengan URL Web App Anda 
  * yang didapat setelah klik 'Deploy' > 'New Deployment' di Google Apps Script.
  */
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzECTRz9Fh0hEUJplqYMgMTVkv5B_1zJ3JQNX0jYe4ce5mkCGfuVOsLVibYg8cvFsGv/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbziIij04_eraKx7dvoG2zBtJqqUnDMpQ8TWB3yFQRjPQ6uZY0rgxTwTU5ZhGcoKXDUm/exec';
 
 export const api = {
   submitRegistration: async (data: StudentRegistration) => {
