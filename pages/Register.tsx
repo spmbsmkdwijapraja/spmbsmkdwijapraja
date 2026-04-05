6aba18f796f49bb67ceae088f52788309708c577
spmbsmkdwijapraja <ppdbsmkdwijapraja@gmail.com>
2026-04-05 12:22:09 +0700

    Update Register.tsx

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -179,6 +179,10 @@ const Register: React.FC = () => {
                   <FormInput label="Asal Sekolah" name="asalSekolah" value={formData.asalSekolah} onChange={handleChange} required />
                   <FormInput label="NPSN Sekolah Asal" name="npsnSekolah" type="number" value={formData.npsnSekolah} onChange={handleChange} required />
                 </div>
+
+                <h4 className="font-bold text-slate-700 mt-8 mb-4 border-b pb-2">Pilihan Jurusan</h4>
+                  <FormSelect label="{Pilihan Jurusan 1" name="pilihanJurusan1" value={formData.pilihanJurusan1} onChange={handleChange} options={MAJOR} required />
+                  <FormSelect label="{Pilihan Jurusan 2" name="pilihanJurusan2" value={formData.pilihanJurusan2} onChange={handleChange} options={MAJOR} required />
                 
                 <h4 className="font-bold text-slate-700 mt-8 mb-4 border-b pb-2">Alamat & Keluarga</h4>
                 <FormInput label="Alamat (Jalan/Dusun)" name="alamat" value={formData.alamat} onChange={handleChange} required type="textarea" rows={2} />
