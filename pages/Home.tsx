38f8d04651e9fa42bec9580472b25acc122c9432
spmbsmkdwijapraja <ppdbsmkdwijapraja@gmail.com>
2026-04-05 14:02:41 +0700

    Update Home.tsx

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -80,8 +80,7 @@ const Home: React.FC = () => {
               </div>
               <div className="space-y-1">
                 <div className="text-3xl md:text-4xl font-black">4</div>
-                <div className="text-xs md:text-sm text-blue-100 uppercase font-bold tracking-widest">Kompetensi Keahlian</div>
-                <div className="text-xs md:text-sm text-blue-100 uppercase font-bold tracking-widest">(DKV, TKL, TO & TPFL)</div>
+                <div className="text-xs md:text-sm text-blue-100 uppercase font-bold tracking-widest">Kompetensi Keahlian (DKV, TKL, TO & TPFL)</div>
               </div>
             </div>
           </div>
