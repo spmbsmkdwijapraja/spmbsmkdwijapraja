750db3c763ce3fc9e102af4bfe58597c28cccd0d
spmbsmkdwijapraja <ppdbsmkdwijapraja@gmail.com>
2026-04-05 18:04:19 +0700

    Update Register.tsx

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -190,8 +190,8 @@ const Register: React.FC = () => {
                 </div>
 
                 <h4 className="font-bold text-slate-700 mt-8 mb-4 border-b pb-2">Pilihan Jurusan</h4>
-                  <FormSelect label="Jurusan 1" name="pilihanJurusan1" value={formData.pilihanJurusan1} onChange={handleChange} options={['Desan Komunikasi Visual (DKV)', 'Teknik Ketenagalistrikan (TKL)', 'Teknik Otomotif (TO)', 'Teknik Pengelasan dan Fabrikasi Logam (TPFL)']} required />
-                  <FormSelect label="Jurusan 2" name="pilihanJurusan2" value={formData.pilihanJurusan2} onChange={handleChange} options={['Desan Komunikasi Visual (DKV)', 'Teknik Ketenagalistrikan (TKL)', 'Teknik Otomotif (TO)', 'Teknik Pengelasan dan Fabrikasi Logam (TPFL)']} required />
+                  <FormSelect label="Jurusan ke-1" name="pilihanJurusan1" value={formData.pilihanJurusan1} onChange={handleChange} options={['Desan Komunikasi Visual (DKV)', 'Teknik Ketenagalistrikan (TKL)', 'Teknik Otomotif (TO)', 'Teknik Pengelasan dan Fabrikasi Logam (TPFL)']} required />
+                  <FormSelect label="Jurusan ke-2" name="pilihanJurusan2" value={formData.pilihanJurusan2} onChange={handleChange} options={['Desan Komunikasi Visual (DKV)', 'Teknik Ketenagalistrikan (TKL)', 'Teknik Otomotif (TO)', 'Teknik Pengelasan dan Fabrikasi Logam (TPFL)']} required />
                                   
                 <h4 className="font-bold text-slate-700 mt-8 mb-4 border-b pb-2">Alamat & Keluarga</h4>
                 <FormInput label="Alamat (Jalan/Dusun)" name="alamat" value={formData.alamat} onChange={handleChange} required type="textarea" rows={2} />
