f623ba973cf49ed08374b66aa392dd0ac8470caa
spmbsmkdwijapraja <ppdbsmkdwijapraja@gmail.com>
2026-04-07 17:36:50 +0700

    Update Register.tsx

--- pages/Register.tsx
+++ pages/Register.tsx
@@ -185,7 +185,6 @@ const Register: React.FC = () => {
         <FormInput label="Asal Sekolah" name="asalSekolah" value={formData.asalSekolah} onChange={handleChange} required />
         <FormInput label="NPSN Sekolah Asal" name="npsnSekolah" type="number" value={formData.npsnSekolah} onChange={handleChange} required />
         <FormInput label="Tahun Lulus" name="tahunLulus" type="number" value={formData.tahunLulus} onChange={handleChange} required />
-
       </div>
 
       {/* ================= ALAMAT ================= */}
@@ -215,9 +214,11 @@ const Register: React.FC = () => {
         <FormInput label="Kecamatan" name="kecamatan" value={formData.kecamatan} onChange={handleChange} required />
         <FormInput label="Kabupaten/Kota" name="kabupatenKota" value={formData.kabupatenKota} onChange={handleChange} required />
         <FormInput label="Kode Pos" name="kodePos" type="number" value={formData.kodePos} onChange={handleChange} required />
+        
         <div className="grid grid-cols-2 gap-4 col-span-1 md:col-span-2">
         <FormSelect label="Status dalam Keluarga" name="statusKeluarga" value={formData.statusKeluarga} onChange={handleChange} options={FAMILY_STATUSES} required />
-
+        </div>
+        
         <div className="grid grid-cols-2 gap-4 col-span-1 md:col-span-2">
           <FormInput label="Anak Ke" name="anakKe" type="number" value={formData.anakKe} onChange={handleChange} required />
           <FormInput label="Jumlah Saudara" name="jumlahSaudara" type="number" value={formData.jumlahSaudara} onChange={handleChange} required />
